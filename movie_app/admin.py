356639f79b3ec7a7a0e8e2d1d75760b7f7789b9e
Felixfelicia <131186447+Felixfelicia@users.noreply.github.com>
2023-07-20 23:35:11 +0600

    Delete movie_app directory

--- movie_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from movie_app.models import Movie, Director, Review
-# Register your models here.
-
-admin.site.register(Movie)
-admin.site.register(Director)
-admin.site.register(Review)
