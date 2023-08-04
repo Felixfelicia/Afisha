5c9b7dc833e4889ed1ee73102e79ea0589222491
Felixfelicia <131186447+Felixfelicia@users.noreply.github.com>
2023-08-04 21:57:39 +0600

    Update urls.py
    5 hw

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -2,8 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.director_list_api_view),
-    path('<int:id>/', views.dir_detail_api_view),
+    path('directors/', views.director_list_api_view),
+    path('directors/<int:id>/', views.dir_detail_api_view),
     path('movies/', views.movie_list_api_view),
     path('movies/<int:id>/', views.movie_detail_api_view),
     path('reviews/', views.review_list_api_view),
