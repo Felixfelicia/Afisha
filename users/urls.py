41b70e63e20abfc2cca8744897e5709c4785b28a
Felixfelicia <131186447+Felixfelicia@users.noreply.github.com>
2023-08-06 23:38:46 +0600

    Delete users directory

--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('authorization/', views.authorization_api_view),
-    path('registration/', views.registration_api_view),
-    path('confirm/', views.user_confirmation_api_view, name='user_confirmation'),
-]
